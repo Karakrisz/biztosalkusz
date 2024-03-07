4216c82afc0727748b49ba22541a2b694ff09dd1
Boaz <webevolution13@gmail.com>
2024-03-07 18:59:45 +0100

    upDate
    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,185 @@
 
 
 @section('content')
+<section>
+    <div class="slider-content">
+
+        <img class="slider-content__img" src="/img/slider/slider.webp" alt="Biztos Alkuszom">
+
+    </div>
+</section>
+
+<section>
+    <div class="partners-content">
+
+        <div class="partners-content__header">
+            <h2 class="partners-content__header__h2 text-color-w text-transform-uppercase">Partnerek</h2>
+        </div>
+
+        <div class="partners-content__img-box grid-7">
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners.svg" alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners2.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners3.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners4.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners5.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners6.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners7.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+        </div>
+
+        <div class="partners-content__img-box grid-7">
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners8.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners9.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners10.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners11.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners12.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners13.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+            <div class="partners-content__img-box__div text-center">
+                <img class="partners-content__img-box__div__img" src="/img/partners/partners14.svg"
+                    alt="Biztos Alkuszom">
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+<section>
+
+    <div class="services-content">
+
+        <div class="servive-grid grid-3">
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title text-transform-uppercase">Gépjármű biztosítás</h3>
+                    <p class="service-box__description">Nyugodt utazást és védelmet nyújt minden kilométeren,
+                        balesetektől kezdve a váratlan meghibásodásokig.</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title text-transform-uppercase">UTASbiztosítás</h3>
+                    <p class="service-box__description">A gondtalan kalandok záloga, amely biztonságot nyújt a világ
+                        bármely pontján, legyen szó váratlan eseményekről vagy egészségügyi sürgősségekről.</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title">Gépjármű biztosítás</h3>
+                    <p class="service-box__description">A tanulóbiztosítás biztos háttér a diákok számára, hogy
+                        nyugodtan összpontosíthassanak tanulmányaikra, miközben védelemben részesülnek az élet váratlan
+                        eseményei ellen.</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="servive-grid grid-3">
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title text-transform-uppercase">RENDEZVÉNYSZOLGÁLAT</h3>
+                    <p class="service-box__description">Biztosításaink magukban foglalják a felelősségbiztosítást, a
+                        rendezvény helyszínének védelmét, valamint az egyedi igényekhez igazodó kockázatkezelést,
+                        biztosítva ezzel a rendezvény zavartalan lebonyolítását..</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title text-transform-uppercase">Kárbejelentés</h3>
+                    <p class="service-box__description">A kárbejelentési folyamatunk során a biztosítási alkusz
+                        szakértői segítséget és támogatást nyújtanak az ügyfeleknek, hogy a kárbejelentés gyorsan és
+                        zökkenőmentesen történjen.</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+            <div class="service-box bg-color-w">
+                <div class="service-box__text">
+                    <h3 class="service-box__title">Személyes tanácsadás</h3>
+                    <p class="service-box__description">személyre szabott tanácsadásunkkal biztosítjuk, hogy minden
+                        ügyfél megkapja az egyéni igényeinek megfelelő biztosítási tanácsokat és megoldásokat,
+                        biztosítva ezzel az optimális védelmet és nyugalmat.</p>
+                </div>
+                <div class="service-box__link-box text-center">
+                    <a class="page-link text-color-w" href="#">Tovább</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
 
 @endsection
