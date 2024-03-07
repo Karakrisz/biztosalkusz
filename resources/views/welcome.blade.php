eab937c5214909f71db761cb01870a0d1e2ef43d
Boaz <webevolution13@gmail.com>
2024-03-07 19:50:15 +0100

    upDate
    front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
 <section>
     <div class="partners-content">
 
-        <div class="partners-content__header">
-            <h2 class="partners-content__header__h2 text-color-w text-transform-uppercase">Partnerek</h2>
+        <div class="page__header">
+            <h2 class="page__header__h2 text-color-w text-transform-uppercase">Partnerek</h2>
         </div>
 
         <div class="partners-content__img-box grid-7">
@@ -99,41 +99,54 @@
 
 <section>
 
-    <div class="services-content">
+    <div class="services-content position-relative">
+
+        <div class="page__header page__header--right">
+            <h3 class="page__header__h3 text-color-w text-transform-uppercase">SZOLGÁLTATÁSOK</h3>
+        </div>
 
         <div class="servive-grid grid-3">
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title text-transform-uppercase">Gépjármű biztosítás</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">Gépjármű biztosítás</h3>
                     <p class="service-box__description">Nyugodt utazást és védelmet nyújt minden kilométeren,
                         balesetektől kezdve a váratlan meghibásodásokig.</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title text-transform-uppercase">UTASbiztosítás</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">UTASbiztosítás</h3>
                     <p class="service-box__description">A gondtalan kalandok záloga, amely biztonságot nyújt a világ
                         bármely pontján, legyen szó váratlan eseményekről vagy egészségügyi sürgősségekről.</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title">Gépjármű biztosítás</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">Gépjármű biztosítás</h3>
                     <p class="service-box__description">A tanulóbiztosítás biztos háttér a diákok számára, hogy
                         nyugodtan összpontosíthassanak tanulmányaikra, miközben védelemben részesülnek az élet váratlan
                         eseményei ellen.</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
@@ -143,42 +156,58 @@
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title text-transform-uppercase">RENDEZVÉNYSZOLGÁLAT</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">RENDEZVÉNYSZOLGÁLAT</h3>
                     <p class="service-box__description">Biztosításaink magukban foglalják a felelősségbiztosítást, a
                         rendezvény helyszínének védelmét, valamint az egyedi igényekhez igazodó kockázatkezelést,
                         biztosítva ezzel a rendezvény zavartalan lebonyolítását..</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title text-transform-uppercase">Kárbejelentés</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">Kárbejelentés</h3>
                     <p class="service-box__description">A kárbejelentési folyamatunk során a biztosítási alkusz
                         szakértői segítséget és támogatást nyújtanak az ügyfeleknek, hogy a kárbejelentés gyorsan és
                         zökkenőmentesen történjen.</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
             <div class="service-box bg-color-w">
                 <div class="service-box__text">
-                    <h3 class="service-box__title">Személyes tanácsadás</h3>
+                    <h3 class="service-box__title text-transform-uppercase text-center">Személyes tanácsadás</h3>
                     <p class="service-box__description">személyre szabott tanácsadásunkkal biztosítjuk, hogy minden
                         ügyfél megkapja az egyéni igényeinek megfelelő biztosítási tanácsokat és megoldásokat,
                         biztosítva ezzel az optimális védelmet és nyugalmat.</p>
                 </div>
                 <div class="service-box__link-box text-center">
-                    <a class="page-link text-color-w" href="#">Tovább</a>
+                    <a class="page-link text-color-w" href="#">Tovább
+                        <img class="page-link__img position-relative" src="/img/partners/right-arrow.svg"
+                            alt="Biztos Alkuszom">
+                    </a>
                 </div>
             </div>
 
         </div>
 
+    </div>
+
+    <div class="service-bottom position-relative">
+        <img class="service-bottom__img" src="/img/partners/bottom.svg" alt="Biztos Alkuszom">
+        <div class="service-bottom__box position-absolute">
+            <h4 class="service-bottom__box__title">"Nálunk a biztosítás találkozik a bizalommal."</h4>
+        </div>
     </div>
 
 </section>
